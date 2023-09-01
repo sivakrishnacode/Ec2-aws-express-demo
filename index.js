664423b10fdecde6c316130e16dacd994bfc713f
sivacartrabbit <siva@cartrabbit.in>
2023-09-01 21:24:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Hello from root! --siva",
+  });
+});
+
+app.get("/path", (req, res) => {
+  return res.status(200).json({
+    message: "Hello from path! --path",
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Running on 3000');
+})
